3476796e6aed554dd2a9a90448429bb1b0444223
Dhruv Shah <dhruvkshah75@gmail.com>
2026-02-11 00:50:28 +0530

    Solved the Variables Ex questions

--- exercises/01_variables/variables5.rs
+++ exercises/01_variables/variables5.rs
@@ -3,6 +3,15 @@ fn main() {
     println!("Spell a number: {number}");
 
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
-    number = 3;
+    // simply overshadow the the above number varaiable 
+    let number: i32 = 3;
     println!("Number plus two is: {}", number + 2);
+
+
+    // to convert a string into a number we do 
+    // let number: u32 = "42".parse().expect("Not a number!") 
+    // we use a expect clause as the string might not be converted into a number
+
+    // let guess: u32 = "42".parse().expect("Not a number!");   => we must put in the datatype of the converted string 
+
 }
